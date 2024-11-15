006854e52905f5f133490ee4ba8767ae20c961e1
him8033 <himanshu8033@gmail.com>
2024-11-15 19:24:45 +0530

    res.send function calling and some type of response are tried

--- index.js
+++ index.js
@@ -9,4 +9,11 @@ app.listen(port, () => {
 
 app.use((req,res) => {
     console.log("request received...");
+    let data="this is a basic response";
+    let data2 = {
+        name : "apple",
+        color : "red"
+    }
+    let data3 = "<h1>Fruits</h1> <ul><li>Apple</li><li>Mango</li></ul>"
+    res.send(data3);
 })
