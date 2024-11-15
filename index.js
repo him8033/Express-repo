da9f267673f0542d2b3d6a0841fcd9a4621c58cf
him8033 <himanshu8033@gmail.com>
2024-11-15 19:44:46 +0530

    app.get and app.post method implementation :

--- index.js
+++ index.js
@@ -7,13 +7,32 @@ app.listen(port, () => {
     console.log(`app is listning on port ${port}`);
 })
 
-app.use((req,res) => {
-    console.log("request received...");
-    let data="this is a basic response";
-    let data2 = {
-        name : "apple",
-        color : "red"
-    }
-    let data3 = "<h1>Fruits</h1> <ul><li>Apple</li><li>Mango</li></ul>"
-    res.send(data3);
+// app.use((req,res) => {
+//     console.log("request received...");
+//     let data="this is a basic response";
+//     let data2 = {
+//         name : "apple",
+//         color : "red"
+//     }
+//     let data3 = "<h1>Fruits</h1> <ul><li>Apple</li><li>Mango</li></ul>"
+//     res.send(data3);
+// })
+
+app.get("/", (req,res) => {
+    res.send("You Contacted root path");
+})
+app.get("/apple", (req,res) => {
+    res.send("You Contacted apple path");
+})
+app.get("/banana", (req,res) => {
+    res.send("You Contacted banana path");
+})
+app.get("/mango", (req,res) => {
+    res.send("You Contacted mango path");
+})
+app.get("/*", (req,res) => {
+    res.send("This path does not exists");
+})
+app.post("/", (req,res) => {
+    res.send("You sent a post request");
 })
